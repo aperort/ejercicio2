6c26de21acdbaf0247a22d6652cd8fe42e92a8ea
Alfonso <aperort316@ieszaidinvergeles.org>
2025-11-13 20:50:52 +0100

    Juego terminao

--- js/Tablero.js
+++ js/Tablero.js
@@ -1,70 +1,61 @@
 import { Submarino } from "./Submarino.js";
-import { Celda } from "./Celda.js";
-import { Vecinos } from "./Celda.js";
 
 class Tablero {
-    constructor() {
+    constructor(UIcontrol) {
         /**
             this.tablero = null;
             this.size = 0;
          */
+
+        this.UIcontrol = UIcontrol;
+        this.UIcontrol.start(this);
         this.submarino = new Submarino();
-        this.status = document.getElementById("status");
         this.size = 0;
         /*console.log("El tablero se ha creado");*/
     }
 
-    creaGridTemplate(total, plantilla, tabla) {
-    this.size = total;
-    
-    this.submarino.x = Math.floor(Math.random() * total);
-    this.submarino.y = Math.floor(Math.random() * total);
-    console.log(`Submarino en: ${this.submarino.x}, ${this.submarino.y}`);
-    
-    tabla.style.gridTemplateColumns = `repeat(${total}, 1fr)`;
-    Array.from({ length: total*total }, (_, i) => {
-        const clon = plantilla.content.cloneNode(true);
-        const casilla = clon.querySelector('.casilla');
-        casilla.textContent = ` ${i%total}, ${Math.floor(i/total)}`;
+    creaGridTemplate(total) {
+        this.size = total;
         
-        casilla.addEventListener('click', (event) => {
-            const filaClick = parseInt(casilla.dataset.fila);
-            const columnaClick = parseInt(casilla.dataset.columna);
-            
-            this.dispara(columnaClick, filaClick, event.target);
-        });
-        
-        casilla.dataset.fila = Math.floor(i/total);
-        casilla.dataset.columna = i%total;
-        tabla.appendChild(clon);
-    });
-}
+        this.submarino.x = Math.floor(Math.random() * total);
+        this.submarino.y = Math.floor(Math.random() * total);
+        console.log(`Submarino en: ${this.submarino.x}, ${this.submarino.y}`);
 
-    /*
-        init(config) {
-        this.size = config.size;
+        const tabla = this.UIcontrol.control.board;
+
+        const plantilla = this.UIcontrol.crearElementos('template');
+        const divCasilla = this.UIcontrol.crearElementos('div');
+        divCasilla.className = 'casilla';
+        plantilla.content.appendChild(divCasilla);
         
-        let malla = Array.from({ length: this.size }, (v, i) => 
-            Array.from({ length: this.size }, (v, j) => new Celda( i,j))
-        );
-        malla.forEach((item,i)=>item.forEach((celda,j)=>{
-            if(i>0) celda.nuevoVecino(Vecinos.ARRIBA,malla[i-1][j]);
-            if(i<this.size-1) celda.nuevoVecino(Vecinos.ABAJO,malla[i+1][j]);
-            if(j>0) celda.nuevoVecino(Vecinos.IZQUIERDA,malla[i][j-1]);
-            if(j<this.size-1) celda.nuevoVecino(Vecinos.DERECHA,malla[i][j+1]);
-        }));
-        this.tablero=malla.flat();
-        this.submarino.init(this.size);
-    };
-    */
+        tabla.style.gridTemplateColumns = `repeat(${total}, 1fr)`;
+        Array.from({ length: total*total }, (_, i) => {
+            const clon = plantilla.content.cloneNode(true);
+            const casilla = clon.querySelector('.casilla');
+
+            casilla.textContent = `${i%total}, ${Math.floor(i/total)}`;
+            
+            casilla.dataset.fila = Math.floor(i/total);
+            casilla.dataset.columna = i%total;
+
+            casilla.addEventListener('click', (event) => {
+                const columnaClick = parseInt(event.target.dataset.columna);
+                const filaClick = parseInt(event.target.dataset.fila);
+                this.dispara(columnaClick, filaClick);
+            });
+            
+            tabla.appendChild(clon);
+        });
+    }
+
 
     dispara(x, y) {
     if(this.submarino.x === x && this.submarino.y === y) {
-        this.status.innerHTML = "¡Le has dado!";
+        this.UIcontrol.changeStatus("Le has dado");
         return true;
     }
     
-    this.status.innerHTML = "No le has dado";
+    this.UIcontrol.changeStatus("no le has dado");
 
     const vecinos = [];
 
